6a5ee7c97818c1393f26363888206796a05d2e9b
BubbleNowrin <nowrin02@gmail.com>
2025-07-22 16:20:48 -0400

    validation added for update user

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,13 +4,13 @@ import { checkAuth } from "../../middlewares/checkAuth";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { userControllers } from "./user.controller";
 import { Role } from "./user.interface";
-import { createUserZodSchema } from "./user.validation";
+import { createUserZodSchema, updateUserZodSchema } from "./user.validation";
 
 const router=Router();
 
 
 router.post("/register",validateRequest(createUserZodSchema),userControllers.createUser);
 router.get("/all-users",checkAuth(Role.ADMIN,Role.SUPER_ADMIN),userControllers.getAllUsers);
-router.patch("/:id",checkAuth(...Object.values(Role)),userControllers.updateUser)
+router.patch("/:id",validateRequest(updateUserZodSchema) ,checkAuth(...Object.values(Role)),userControllers.updateUser)
 
 export const userRoutes=router;
